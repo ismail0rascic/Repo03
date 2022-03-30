32763b143312ed73e7795d48fe12134e0d33bf5f
“ismail0rascic” <ismail0rascic.gmail.com>
2022-03-30 23:25:58 +0200

    index

---
+++ ex02/src/index.js
@@ -0,0 +1,13 @@
+function main() {
+    //This is single line JavaScript comment
+
+    /* document.write("this is a multi-linecomment!")
+    document.write("this is a multi-linecomment!")
+    document.write("this is a multi-linecomment!")
+    document.write("this is a multi-linecomment!")
+    document.write("this is a multi-linecomment!")
+    */
+
+}
+console.log(main());
+module.exports = main;
---
+++ ex04/src/index.js
@@ -0,0 +1,10 @@
+function main() {
+    var x;
+    x = 10;
+    var y;
+    y = x;
+    return [x, y]
+}
+console.log(main());
+
+module.exports = main;
